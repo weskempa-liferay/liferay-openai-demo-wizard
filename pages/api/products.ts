1645b1ae1544a9674d66b99d8fe163637c3af4b5
Wesley Kempa <Wesley.Kempa@liferay.com>
2023-11-10 13:19:42 -0500

    Product API testing

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,18 +1,99 @@
 import { Configuration, OpenAIApi } from "openai";
 
+
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
+
+
+  const axios = require("axios");
+
+  let apiPath = "http://localhost:8080/o/headless-admin-taxonomy/v1.0/sites/20120/taxonomy-vocabularies";
+  let vocabPostObj = {'name': req.body.product + ' Type'};
+  let headers = {
+    'Authorization': 'Basic ' + btoa('test@liferay.com:test'), 
+    'Content-Type': 'application/json'
+  };
+  let apiRes = "";
+
+  axios.post(apiPath,
+    vocabPostObj, 
+    headers).then(
+    function (response) {
+      console.log(response);
+      apiRes = response;
+    })
+    .catch(function (error) {
+      console.log(error);
+      apiRes = error;
+    });
+
+  res.status(200).json({ result: apiRes });
+
+  /*const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: reviewPrompt(req.body.product),
     max_tokens: 2000,
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
+
+  let productData = JSON.parse(completion.data.choices[0].text).categories;
+  
+  let productCategories = [];
+
+  for(let i = 0; productData.length>i; i++){
+    productCategories.push(productData[i].categoryName);
+  }
+
+  let categoryDataStr =  {
+    "Category Vocab": req.body.product + " Type",
+    "Category Names": productCategories
+  } 
+
+  console.log(categoryDataStr);
+
+*/
+/*
+  //const https = require("https");
+  const http = require('node:http');
+
+  //http://localhost:8080/o/headless-admin-taxonomy/v1.0/sites/20120/taxonomy-vocabularies
+
+  const options = {
+    hostname: 'localhost',
+    port: 8080,
+    path: '/o/headless-admin-taxonomy/v1.0/sites/20120/taxonomy-vocabularies',
+    method: 'POST',
+    headers: new Headers({
+      'Authorization': 'Basic ' + btoa('test@liferay.com:portal4all'), 
+      'Content-Type': 'application/json'
+    })
+  }
+  
+  const request = http
+    .request(options, resp => {
+      // log the data
+      resp.on("data", d => {
+        console.log("DATA: "+d);
+      });
+    })
+    .on("error", err => {
+      console.log("Error: " + err.message);
+    });
+
+    request.write("{'name':'"+req.body.product + " Type'}");
+    request.end();
+
+
+    console.log("End of code");
+    */
+
+
 }
 
 function reviewPrompt(productName) {

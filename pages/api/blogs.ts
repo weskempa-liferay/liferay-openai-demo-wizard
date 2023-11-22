76ac8fa81b0eb45085d9a56f4c68ebf046346fe0
Wesley Kempa <Wesley.Kempa@liferay.com>
2023-11-22 14:22:38 -0500

    Upgrade Open AI - Change blogs to model gpt-3.5-turbo

--- pages/api/blogs.ts
+++ pages/api/blogs.ts
@@ -1,9 +1,8 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI  from "openai";
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
 
@@ -11,8 +10,7 @@ export default async function (req, res) {
   const runCount = req.body.blogNumber;
   const runCountMax = 10;
 
-  let completion;
-  let blogJson, apiPath, blogResponse;
+  let blogJson, blogResponse, response;
   const axios = require("axios");
   const blogContentSet = [];
 
@@ -29,51 +27,74 @@ export default async function (req, res) {
     }
   };
 
+  const schema = {
+    type: "object",
+    properties: {
+      headline: {
+        type: "string",
+        description: "The title of the blog artcile"
+      },
+      alternativeHeadline: {
+        type: "string",
+        description: "A headline that is a summary of the blog article"
+      },
+      articleBody: {
+        type: "string",
+        description: "The content of the blog article. Remove any double quotes",
+      },
+      picture_description: {
+        type: "string",
+        description: "A description of an appropriate image for this blog in six sentences."
+      }
+    },
+    required: ["headline", "alternativeHeadline", "articleBody", "picture_description"]
+  }
+
   for(let i=0; i<runCount; i++){
-    completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: reviewPrompt(req.body.blogTopic),
+    response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "system", "content": "You are a blog author."},
+        {"role": "user", "content": "Write a blog on the subject of: "+req.body.blogTopic}
+      ],
+      functions: [
+        {name: "get_blog_content", "parameters": schema}
+      ],
+      function_call: {name: "get_blog_content"},
       max_tokens: 1000,
       temperature: 0.8,
-      top_p: 1.0,
       frequency_penalty: 0.5,
-      presence_penalty: 0.0
+      presence_penalty: 0
     });
 
-    blogJson = JSON.parse(completion.data.choices[0].text);
+    //console.log(response.choices[0].message.function_call.arguments);
+    //console.log(JSON.parse(response.choices[0].message.function_call.arguments));
+  
+    blogJson = JSON.parse(response.choices[0].message.function_call.arguments);
+
+    let pictureDescription = blogJson.picture_description;
+    delete blogJson.picture_description;
+
+    blogJson.articleBody = blogJson.articleBody.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
+    //console.log("pictureDescription" + pictureDescription)
+
     blogContentSet.push(blogJson);
 
     if(i>=runCountMax)break;
   }
 
   console.log(blogContentSet);
+  console.log(JSON.stringify(blogContentSet));
 
-    try {
-      apiPath = process.env.LIFERAY_PATH + "/o/headless-delivery/v1.0/sites/"+req.body.siteId+"/blog-postings/batch";
+  try {
+    let apiPath = process.env.LIFERAY_PATH + "/o/headless-delivery/v1.0/sites/"+req.body.siteId+"/blog-postings/batch";
 
-      blogResponse = await axios.post(apiPath, blogContentSet, headerObj);
-    }
-    catch(apiError) {
-      console.log("error creating prblogoduct " + req.body.blogTopic + " -- " + apiError);
-    }
+    blogResponse = await axios.post(apiPath, blogContentSet, headerObj);
+  }
+  catch(apiError) {
+    console.log("error creating prblogoduct " + req.body.blogTopic + " -- " + apiError);
+  }
 
   res.status(200).json({ result: JSON.stringify(blogContentSet) });
 }
-
-function reviewPrompt(productName) {
-  return `Topic: Breakfast
-  json blog post: {
-    "alternativeHeadline": "Sarah enjoyed a peaceful breakfast as a comforting ritual.",
-    "articleBody": "As the morning sun painted the kitchen in warm hues, Sarah savored the simple joy of breakfast – the aroma of freshly brewed coffee and the crisp sound of bacon sizzling in the pan. Amidst the routine, she found a moment of serenity, a daily ritual that turned ordinary mornings into a comforting embrace of flavors and quiet reflection.",
-    "headline": "Sarah' Breakfast Ritual"
-  }
-  Topic: ${productName}
-  json blog post:`;
-}
-
-
-
-
-
-
-

51822d7a5581f1261eab666b1f61d0d0b0b6d3bc
Wesley Kempa <wesley.kempa@liferay.com>
2023-12-20 19:09:36 -0500

    Introducing Form Field Components - User AI Page UI Cleanup

--- pages/users-ai.tsx
+++ pages/users-ai.tsx
@@ -5,6 +5,8 @@ import AppHeader from "./components/appheader";
 import AppFooter from "./components/appfooter";
 import LoadingAnimation from "./components/loadinganimation";
 import ResultDisplay from "./components/resultdisplay";
+import FieldString from "./components/formfield-string";
+import FieldSubmit from "./components/formfield-submit";
 
 import hljs from "highlight.js";
 
@@ -12,19 +14,13 @@ export default function Review() {
 
   const [userNumberInput, setUserNumberInput] = useState("5");
   const [emailPrefixInput, setEmailPrefixInput] = useState("liferay.xyz");
-  
-  const [userImageToggle, setUserImageToggle] = useState(true);
+
   const [result, setResult] = useState(() => "");
   const [isLoading, setIsLoading] = useState(false);
-
   const [debugMode, setDebugMode] = useState(false);
 
   const onDebugModeChange = (value) => {
     setDebugMode(value);
-  };
-
-  const handleChange = () => {
-    setUserImageToggle(!userImageToggle);
   };
 
   async function onSubmit(event) {
@@ -38,7 +34,6 @@ export default function Review() {
       body: JSON.stringify({
         userNumber: userNumberInput,
         emailPrefix: emailPrefixInput,
-        includeImages: userImageToggle,
         debugMode: debugMode
       }),
     });
@@ -63,49 +58,26 @@ export default function Review() {
 
           <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:gap-4 mb-5">
 
-            <label className="flex max-w-xs flex-col text-slate-200">
-              Number of Users to Create
-              <input
-                className="text-sm text-gray-base w-full 
-                                  py-5 px-4 h-2 border 
-                                  border-gray-200 text-slate-700 rounded"
-                type="text"
-                name="userNumber"
-                placeholder="Number of user posts"
-                value={userNumberInput}
-                onChange={(e) => setUserNumberInput(e.target.value)}
-              />
-            </label>
+            <FieldString 
+                    name={"userNumber"}
+                    label={"Number of Users to Create"} 
+                    placeholder={"Number of users"}
+                    inputChange={setUserNumberInput}
+                    defaultValue={""}
+                  />
 
-            <label className="flex max-w-xs flex-col text-slate-200">
-              Email prefix
-              <input
-                className="text-sm text-gray-base w-full 
-                                  py-5 px-4 h-2 border 
-                                  border-gray-200 text-slate-700 rounded"
-                type="text"
-                name="userNumber"
-                placeholder="@liferay.xyz"
-                value={emailPrefixInput}
-                onChange={(e) => setEmailPrefixInput(e.target.value)}
-              />
-            </label>
-            
-            <label className="imgtoggle elative  inline-flex items-center cursor-pointer hidden">
-              <input type="checkbox" checked={userImageToggle} onChange={handleChange} value="" className="sr-only peer"/>
-              <div className="absolute w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Generate Profile Pictures</span>
-            </label>
+            <FieldString 
+                    name={"emailPrefix"}
+                    label={"Email Prefix"} 
+                    placeholder={"@liferay.xyz"}
+                    inputChange={setEmailPrefixInput}
+                    defaultValue={"@liferay.xyz"}
+                  />
             
           </div>
-          
-          <button disabled={isLoading}
-            className="text-sm w-full bg-blue-600 h-10 text-white
-                              rounded-2xl mb-10"
-            type="submit"
-          >
-            Generate Users
-          </button>
+
+          <FieldSubmit label={"Generate Users"} disabled={isLoading} />
+
         </form>
 
         {isLoading ? (

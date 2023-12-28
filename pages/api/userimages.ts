1aa8ba218a08d952c8d896eb12a6787a3818fdee
Wesley Kempa <wesley.kempa@liferay.com>
2023-12-28 15:22:53 -0500

    Updates to user generation options
    Update to user images
    update to use csv to even out male / female ratio
    Add User images to AI generation

--- pages/api/userimages.ts
+++ pages/api/userimages.ts
@@ -1,17 +1,17 @@
 export default async function (req, res) {
 
-    const folder = process.cwd()+"/public/images/art-styles/";
+    const folder = process.cwd()+"/public/users/user-images/";
     const fs = require('fs');
 
     let styleList = [];
-
+  
     fs.readdir(folder, (err, files) => {
         files.forEach(file => {
-            if(file.startsWith('style'))
+            if(file.startsWith(req.body.gender))
                 styleList.push(file);
         });
         
-        res.status(200).json({ result: JSON.stringify(styleList) });
+        res.status(200).json({ result: styleList[ req.body.index % 6 ] });
     });
-  
+
 }
--- pages/api/users-ai.ts
+++ pages/api/users-ai.ts
@@ -1,6 +1,9 @@
 import OpenAI  from "openai";
 
 var functions = require('../utils/functions');
+const fs = require('fs');
+const axios = require("axios");
+const request = require('request');
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -29,22 +32,23 @@ export default async function (req, res) {
                 },
                 familyName:{
                   type: "string",
-                  description: "The user's last name"
+                  description: "The user's last name. Do not use the name Smith or Doe."
                 },
                 givenName:{
                   type: "string",
-                  description: "The user's first name"
+                  description: "The user's first name. Do not use the names Jane or John."
                 },
-                middleName:{
+                gender:{
                   type: "string",
-                  description: "This is the user's middle name."
+                  enum: ["male", "female"],
+                  description: "This is the user's gender."
                 },
                 jobTitle:{
                   type: "string",
                   description: "The user's job title."
                 }
               },
-              required: [ "birthDate", "familyName", "givenName", "middlelName", "jobTitle"]
+              required: [ "birthDate", "familyName", "givenName", "gender", "jobTitle"]
             },
             required: ["users"]
           }
@@ -55,7 +59,7 @@ export default async function (req, res) {
         model: "gpt-3.5-turbo",
         messages: [
             {"role": "system", "content": "You are a system administrator responsible for adding users to a portal."},
-            {"role": "user", "content": "Create a list of example users to be added to the portal for demonstration. Return only the result of the get_users function."}
+            {"role": "user", "content": "Create a list of example users to be added to the portal for demonstration. Do not use the first or last names John, Jane, Smith, or Doe. Return only the result of the get_users function."}
         ],
         functions: [
             {name: "get_users", "parameters": userSchema}
@@ -64,29 +68,50 @@ export default async function (req, res) {
     });
 
     let userlist = JSON.parse(response.choices[0].message.function_call.arguments).users;
-    
+    let genderCount = {
+      male:0,
+      female:0
+    };
+
     for(let i=0;i<userlist.length;i++){
-        userlist[i].additionalName = userlist[i].middleName;
-        delete userlist[i].middleName;
+
+        console.log(userlist[i].gender);
+
+        let gender = userlist[i].gender;
+        genderCount[gender] = genderCount[gender] + 1;
+        delete userlist[i].gender;
+
         userlist[i].alternateName = userlist[i].givenName+"."+userlist[i].familyName;
         userlist[i].emailAddress = userlist[i].givenName+"."+userlist[i].familyName+"@"+req.body.emailPrefix;
         userlist[i].password = req.body.password;
-    }
-
-    if(debug) console.log(userlist);
-
-    const axios = require("axios");
-
-    let userApiPath = process.env.LIFERAY_PATH + "/o/headless-admin-user/v1.0/user-accounts";
-
-    const options = functions.getAPIOptions("POST","en-US");
-
-    for(let i=0;i<userlist.length;i++){
+  
       try {
-          const response = await axios.post(userApiPath, userlist[i], options);
 
-          if(debug) console.log("Created user:"+response.data.id+", "+response.data.alternateName);
-          successCount ++;
+        const options = functions.getAPIOptions("POST","en-US");
+
+        let userApiPath = process.env.LIFERAY_PATH + "/o/headless-admin-user/v1.0/user-accounts";
+        const response = await axios.post(userApiPath, userlist[i], options);
+
+        if(debug) console.log("Created user:"+response.data.id+", "+response.data.alternateName);
+
+        let userImageApiPath = process.env.LIFERAY_PATH + "/o/headless-admin-user/v1.0/user-accounts/" + response.data.id + "/image";
+        let userImagePath = await getImagePath(gender,genderCount[gender]);
+
+        if(debug) console.log("userImageApiPath:"+userImageApiPath);
+        if(debug) console.log("userImagePath:"+userImagePath);
+        if(debug) console.log(process.cwd() + "/public/users/user-images/" + userImagePath);
+        
+        let fileStream = fs.createReadStream(process.cwd() + "/public/users/user-images/" + userImagePath);
+        const imgoptions = functions.getFilePostOptions(userImageApiPath, fileStream, "image");
+        
+        request(imgoptions, function (err, res, body) {
+            if(err) console.log(err);
+
+            if(debug) console.log("Image Upload Complete");
+        });
+
+
+        successCount ++;
       }
       catch (error) {
         errorCount ++;
@@ -101,4 +126,35 @@ export default async function (req, res) {
       errorCount + " errors in " +
       functions.millisToMinutesAndSeconds(end - start)
   });
+}
+
+async function getImagePath(gender,index){
+
+    let options = {
+      gender: gender,
+      index: index
+    }
+
+    let res = await axios.post("http://localhost:3000/api/userimages", options);
+
+    return res.data.result;
+
+    /*
+    const folder = process.cwd()+"/public/users/user-images/";
+    const fs = require('fs');
+
+    let styleList = [];
+
+    fs.readdir(folder, (err, files) => {
+        files.forEach(file => {
+            if(file.startsWith(gender))
+                styleList.push(file);
+        });
+
+        console.log(index+":"+styleList.length);
+        
+        return styleList[index];
+    });
+    */
+      
 }

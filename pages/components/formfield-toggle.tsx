17d12d93bc7bfffaf90f688bcc967bbef7aba26c
Wesley Kempa <wesley.kempa@liferay.com>
2024-01-03 17:16:09 -0500

    Update formfield-toggle.tsx

--- pages/components/formfield-toggle.tsx
+++ pages/components/formfield-toggle.tsx
@@ -31,7 +31,7 @@ export default function FieldToggle({
         type="checkbox"
       />
       <div className="absolute w-11 h-6 bg-gray-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-      <span className="ms-1 text-sm font-medium text-gray-900 dark:text-gray-300">
+      <span className="ms-1 text-sm font-medium text-white">
         {name}
       </span>
     </label>

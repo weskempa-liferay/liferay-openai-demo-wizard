61da21c4cb3f035a1d82ef013085c12c4d4abf9f
Keven Leone <keven.leone@liferay.com>
2024-03-11 07:55:55 -0300

    Create Axios Service and Utils

---
+++ services/next.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+import { APP_CONFIG } from '../components/layout/footer';
+
+const nextAxios = axios.create();
+
+nextAxios.interceptors.request.use((request) => {
+  const cookies = new Cookies();
+
+  request.data.config = cookies.get(APP_CONFIG);
+
+  return request;
+});
+
+export default nextAxios;

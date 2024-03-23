920b7f7451efe438fd0b802ae9d02073a56f01d0
Keven Leone <keven.leone@liferay.com>
2024-03-23 10:54:53 -0300

    Create FormData Util to download files

--- utils/download.ts
+++ utils/download.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const downloadFile = ({ fileName, filePath }) => {
   const a = document.createElement('a');
   a.download = fileName;
@@ -10,3 +12,18 @@ export const downloadFile = ({ fileName, filePath }) => {
   a.dispatchEvent(clickEvt);
   a.remove();
 };
+
+export async function getDownloadFormData(url: string) {
+  const response = await axios.get(url, {
+    responseType: 'arraybuffer',
+  });
+
+  const blob = new Blob([response.data]);
+  const file = new File([blob], `openai-wizard-${new Date().getTime()}.jpg`);
+
+  const formData = new FormData();
+
+  formData.append('file', file);
+
+  return formData;
+}
